2d766409c0705665de8137f1c3315599895d8eb7
Pavel Břoušek <brousek@ics.muni.cz>
2023-11-14 13:00:41 +0100

    ci: accept install

--- run_pester_tests.ps1
+++ run_pester_tests.ps1
@@ -1,4 +1,4 @@
-Install-Module -Name Pester
+Install-Module -Name Pester -Confirm -AcceptLicense
 Import-Module "Pester" -Force
 Get-Module Pester | select name,version
 exit (Invoke-Pester . -Passthru).FailedCount
